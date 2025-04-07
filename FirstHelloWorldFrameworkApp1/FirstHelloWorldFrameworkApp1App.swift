b1c814c28c90fc027ce5f070da9d26470c3614d3
Daryl R Cox <dcox@justmacapps.net>
2025-04-07 08:01:19 -0500

    Initial Commit

---
+++ FirstHelloWorldFrameworkApp1/FirstHelloWorldFrameworkApp1App.swift
@@ -0,0 +1,17 @@
+//
+//  FirstHelloWorldFrameworkApp1App.swift
+//  FirstHelloWorldFrameworkApp1
+//
+//  Created by Daryl Cox on 4/7/25.
+//
+
+import SwiftUI
+
+@main
+struct FirstHelloWorldFrameworkApp1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
